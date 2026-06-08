ca8a6d086c058378177a5eb59058216c00f57872
NumenTrades <numen.empanadas@gmail.com>
2026-06-08 10:29:02 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function App() {
+  return <div style={{background:'#080c10',color:'#00d4ff',height:'100vh',display:'flex',alignItems:'center',justifyContent:'center',fontFamily:'monospace',fontSize:24}}>Trading Journal cargando...</div>;
+}
